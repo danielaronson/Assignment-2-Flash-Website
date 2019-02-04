e42bc9eaf3c7d1ff45a84f65030916341a9cdf9c
Joshua Dennis <jdenn11@u.rochester.edu>
2019-02-04 10:35:22 -0500

    Added my lab 1

---
+++ lab01/early_days.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Early Days | Joshua Dennis | Lab 1: Initial Personal Website</title>
+	<link rel="stylesheet" href="css/styles.css">
+	<link rel="stylesheet" href="css/navigation.css">
+</head>
+<body class="container">
+	<?php include "inc/top.inc"; ?>
+	<main>
+		<h2>Early Days</h2>
+		<p>I was born in Orlando, Florida on August 14, 1998 and have two siblings, an older brother and a younger sister. After about three years in Florida, my family moved to Lindfield, New South Wales, Australia due to my father’s work. Although I was quite young for my entire time in Australia, it provided some very happy memories nonetheless and will always hold a special place in my heart. I remained overseas until my father’s job caused us to move back to the United States after about four years in Lindfield. We eventually settled in the small town of Lansing, New York, which I am fortunate enough to call my home currently.</p>
+	</main>
+	<?php include "inc/bottom.inc"; ?>
