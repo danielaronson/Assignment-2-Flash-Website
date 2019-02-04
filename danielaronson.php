6f9aa233005100916ef7a6182932cf60f1137047
Eric Feirouz <efeirouz@u.rochester.edu>
2019-02-04 10:51:02 -0500

    Condensed to 3 webpages
    danny likes eggs

---
+++ danielaronson.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+	<title>Daniel B Aronson | Lab 1 </title>
+	<link rel="stylesheet" href="css/styles.css">
+</head>
+<body class="container">
+
+<?php include "inc/head/inc"; ?>
+
+	<header>
+		<a>
+
+			<h1>Daniel B Aronson</h1>
+			<img src="images/Aronson.jpg">
+
+		</a>
+
+
+	</header>
+
+	<aside>
+
+		<ul>
+			<li>Mechanical Engineering Major</li>
+			<li>Computer Science Minor</li>
+			<li>Varsity Athlete</li>
+
+
+		</ul>
+
+	</aside>
+
+	<main>
+		<h2></h2>
+
+		<p>
+
+		I am a senior at the University of Rochester majoring in Mechanical Engineering with a minor in Computer Science. At the University, I am a member of the men’s varsity Swimming and Diving team. I have been a captain of the team for the past two years and compete in sprint freestyle events. I am also a member of the school’s Varsity Student Athlete Advisory Committee (VSAAC). Outside of these activities, I enjoy playing Xbox and golfing. 
+
+
+		</p>
+
+
+		<h2>Early Days</h2>
+
+		<p>
+
+		I was born in Cherry Hill, New Jersey in 1997. I have two sisters, one older and one younger. I went to Johnson Elementary School, then Rosa Middle School, and then Cherry Hill East High School. During the summers, I would swim at Charleston swim club, a neighborhood pool close to my house, and began life guarding there when I was old enough.
+
+		When I was younger, I was interested in athletics and participated in every sport possible. I eventually weeded down my athletic interests to baseball and swimming. I learned to play the guitar, but just as quickly forgot everything that I learned after stopping lessons. In school, I had an interest in STEM courses from the start, although my high school did not offer many courses (or any) in computer sciences. Despite this, I decided to pursue a Computer Science minor after a few years in college. I decided to study Mechanical Engineering in college because of my interest in STEM classes, especially Physics.
+
+		</p>
+
+		<h2>School</h2>
+
+		<p>
+
+		My favorite classes in college so far have been Thermodynamics, Heat Transfer and Material Science. My least favorite class in college was Electricity & Magnetism. For my senior design project this spring, my group and I will be doing athletic helmet testing. We will develop a new testing system which will help researchers try to limit brain injuries in contact sports like football or boxing.
+
+		</p>
+
+
+		<h2>Work</h2>
+
+		<p>
+
+		After my sophomore year of college, I began working for a company called Marathon Engineering & Environmental Services, which was a civil and environmental engineering firm. I interned there for a summer. My work consisted of assisting surveyors with various duties. This included both elevation mapping of a work cite and wetlands mapping. This work often meant trekking through the woods on 100 degree days with a machete trying to map wetland boundaries. After my Junior year of college, I secured an internship with Wegman’s as a member of their design services team. This meant that I assisted with any engineering work that went on within the stores themselves. I helped plan new Solar Panel Systems for their stores, helped plan circuitry layout for some of their displays, and assisted in designing a new Condenser screen, among many other things. After graduation, I will be working for Lockheed Martin and pursuing a Graduate Degree in Mechanical Engineering. My program will allow me to work in various different roles within the company. This will allow me to gain expertise in different fields before I eventually settle into one role.
+
+
+
+	</main>
+
+	<footer>CSC 174: Advanced Front-end Web Design and Development</footer>
+
+	<?php include "inc/nav.inc"; ?>
+
+
+<script src="//code.jquery.com/jquery.js"></script>
+<script src="js/menu-highlighter.js"></script>
+</body>
+</html>
---
+++ ericfeirouz.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>After College|Eric B Feirouz|Lab1: Initial Personal Website</title>
+	<link rel="stylesheet" href="css/styles.css">
+</head>
+<body class="container">
+	<header>
+	<a href="index.php">
+
+		<h1>Eric Feirouz</h1>
+		<img src="images/feirouz.jpg" alt="Eric Feirouz Roster Pic">
+		</a>
+		
+
+
+	</header>
+
+	<?php include "includes/list.inc";?>
+
+
+	<main>
+	<h2> Before U of R</h2>
+
+	<p>
+	I am currently a senior at the University of Rochester. I grew up in Acton, Massachusetts, a small town right outside of Boston. I have a sibling who is currently in 7th grade. Before college, I enjoyed playing video games, watching movies and playing sports with my friends. During my senior year of highschool, not having a particular major in mind, I chose to attend the University of Rochester due to how well rounded all of its programs are. On top of this, there was a spot for me on the swim team. I have been a competitive swimmer since I was 8 years old, so swimming was a big part of the decision to attend here. I was also used to heavy winters, so I did not let the Rochester weather deter me from committing here.
+	</p>
+
+	<h2> Computer Science Minor</h2>
+	<p>
+	When deciding on my major I was torn between Engineering and Computer Science. I was intrigued by engineering due to my deep interest in physics in high school. However, I knew computer science was a fascinating major as it required a substantial amount of logic and creativity. It took me 2 semesters to finalize on mechanical engineering, but I made sure to pursue a minor in computer science due to the fun I had in previous coding course I had taken. At the University of Rochester, I have taken: Intro to Programming, Data Structures, Web Design and Development and Statistical Programming. This semester I am taking Advanced Web Design and Robot Construction.
+	</p>
+
+	<h2> Mechanical Engineering Major</h2>
+	<p>
+	In regards to engineering coursework, I have loved the hands on experience I have gotten from my mechanical design class. I also enjoyed fluid dynamics and thermodynamics. For engineering internships, I spent the summer of 2017 in Rochester working for Nalco Water. This was a client facing role that involved working with various heating plants in the upstate New York area. I worked the summer of 2018 in Vermont for Keurig Dr Pepper as a Quality Engineering Intern. I worked on an engineering project, and was also able to work on a side project that involved SQL and Databases. I was able to work on the side project after expressing my interest in coding and big data. I thoroughly enjoyed my internship this summer. I also loved living in Burlington, Vermont. I had 4 other roommates that are all seniors at the University of Vermont. Over the summer, I frequently visited lake champlain and ate at all the great restaurants Burlington has to offer. Best of all, I received a lot of free coffee from Keurig.
+	</p>
+
+	
+	<h2> Plans After College</h2>
+	<p>
+	For my senior spring semester, I hope to explore Rochester as much as I can before I graduate. After graduation, I will be working as an analyst for Deloitte in the Washington D.C area. I will hope to traveland visit places that I have never visited before and spend time with my family before I move to D.C. I look forward to see what the next few years have in store for me!
+	</p>
+
+
+
+
+	</main>
+
+	<?php include "includes/footer.inc";?>
+
+	<?php include "includes/nav.inc";?>
+
+		<script src="//code.jquery.com/jquery.js"></script>
+		<script src="js/menu-highlighter.js"></script>
+	</body>
+
+	</html>
